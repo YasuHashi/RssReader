a7473509ca558b5cb56d4d24915a7ae827026898
YasuHashi <hashitani6@outlook.jp>
2018-04-03 14:32:44 +0900

    ブックマークの追加
    ブックマークのアイコンを追加した
    
    Co-Authored-By: yasuhashi <yasuhashi@users.noreply.github.com>

---
+++ RssReader/BookmarkViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookmarkViewController.swift
+//  RssReader
+//
+//  Created by sonicmoov on 2018/04/03.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class BookmarkViewController: UITableViewController {
+    
+}
